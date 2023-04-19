0f4cc7336c2dfd6f39ed0b3287e0809c9cc3a680
duressa-feyissa <duresafeyisa2022@gmail.com>
2023-04-19 18:31:19 +0300

    progress

--- api/startUp/config.js
+++
@@ -1,9 +0,0 @@
-const config = require('config'); 
-
-module.exports = function() {
-  if (!config.get('API_Private_Key')) {
-
-    throw new Error('FATAL ERROR: API_Private_Key.');
-    process.exit(1); 
-  }  
-}
--- api/startUp/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const url = 'mongodb://localhost:27017/A2A';
-const options = { useNewUrlParser: true, useUnifiedTopology: true };
-
-module.exports = function() {
-  mongoose.connect(url, options)
-  .then(() => console.log('Connected to MongoDB...'))
-  .catch((err) => console.error('Failed to connect to MongoDB:', err));
-}
